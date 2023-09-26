fd8bb80a4d959b3729c1cf46e1ff1d13c3e35f93
Demi <114027417+DemiScript@users.noreply.github.com>
2023-09-26 13:58:03 -0400

    Assignment 1

---
+++ components/Hello.js
@@ -0,0 +1,12 @@
+import React from'react';
+import Link from 'next/link';
+
+const Hello = ({}) => {
+    return(
+        <div className="items-center">
+            <h1>Welcome to my app!</h1>
+        </div>
+    );
+};
+
+export default Hello;
